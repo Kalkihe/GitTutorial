bf8a9a2d0b94c9b066a868c8570eaf534773a4da
Kalkihe <malina.thomas@student.dhbw-karlsruhe.de>
2018-10-25 09:28:05 +0200

    implemented asking for name and greeting

--- src/GitTutorial.java
+++ src/GitTutorial.java
@@ -1,2 +1,9 @@
 public class GitTutorial {
+    public static void main(String Args[])
+    {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("Bitte Namen eingeben: ");
+        String name = scanner.nextLine();
+        System.out.println("Hallo " + name);
+    }
 }
